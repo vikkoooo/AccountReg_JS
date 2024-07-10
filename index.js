60580105db5f72fa3c01134ba543e9464acbe9dd
vikkoooo <vikkolundberg@gmail.com>
2024-07-10 12:13:43 +0200

    toggle helper texts with toggle function and adding listeners through loop

--- index.js
+++ index.js
@@ -1,79 +1,39 @@
 // Add references
 // Input fields
+const inputs = document.querySelectorAll("input");
 const nameInput = document.querySelector(`input[name="name"]`);
 const usernameInput = document.querySelector(`input[name="username"]`);
 const emailInput = document.querySelector(`input[name="email"]`);
 const passwordInput = document.querySelector(`input[name="password"]`);
 const confirmPasswordInput = document.querySelector(`input[name="password_two"]`);
 
-// Helper text fields
-const nameHelp = document.querySelector(".nameHelp");
-const usernameHelp = document.querySelector(".usernameHelp");
-const emailHelp = document.querySelector(".emailHelp");
-const passwordHelp = document.querySelector(".passwordHelp");
-const confirmPasswordHelp = document.querySelector(".confirmPasswordHelp");
+// Help texts
+const helpTexts = document.querySelectorAll(".helpText");
 
 // Form and button
 const registerForm = document.querySelector(".registerForm");
 const submitButton = document.querySelector(".submitButton");
 
-// Add show/hide helpers on focus or blue (defocus)
-// Name
-nameInput.addEventListener("focus", () => {
-	// normal default behaviour
-	nameHelp.classList.remove("hide");
-	nameHelp.classList.add("show");
+// Add eventlisteners on focus and blur for all input fields
+inputs.forEach(input => {
+	input.addEventListener("focus", toggleHelpText);
+	input.addEventListener("blur", toggleHelpText);
 });
 
-nameInput.addEventListener("blur", () => {
-	// hides element
-	nameHelp.classList.add("hide");
-	nameHelp.classList.remove("show");
-});
+// Function to toggle help text visibility
+function toggleHelpText(e) {
+	const parent = e.target.parentNode; // will return the <label> and its children
+	const helpTextNode = parent.querySelector(".helpText"); // will grab our <p>
 
-// Username
-usernameInput.addEventListener("focus", () => {
-	usernameHelp.classList.remove("hide");
-	usernameHelp.classList.add("show");
-});
-
-usernameInput.addEventListener("blur", () => {
-	usernameHelp.classList.add("hide");
-	usernameHelp.classList.remove("show");
-});
-
-// Email
-emailInput.addEventListener("focus", () => {
-	emailHelp.classList.remove("hide");
-	emailHelp.classList.add("show");
-});
-
-emailInput.addEventListener("blur", () => {
-	emailHelp.classList.add("hide");
-	emailHelp.classList.remove("show");
-});
-
-// Password
-passwordInput.addEventListener("focus", () => {
-	passwordHelp.classList.remove("hide");
-	passwordHelp.classList.add("show");
-});
-
-passwordInput.addEventListener("blur", () => {
-	passwordHelp.classList.add("hide");
-	passwordHelp.classList.remove("show");
-});
-
-// Confirm password
-confirmPasswordInput.addEventListener("focus", () => {
-	confirmPasswordHelp.classList.remove("hide");
-	confirmPasswordHelp.classList.add("show");
-});
-
-confirmPasswordInput.addEventListener("blur", () => {
-	confirmPasswordHelp.classList.add("hide");
-	confirmPasswordHelp.classList.remove("show");
-});
+	// type tells us if we did focus or unfocus (blur)
+	if (e.type === "focus") {
+		helpTextNode.classList.remove("hide");
+		helpTextNode.classList.add("show");
+	} else if (e.type === "blur") {
+		helpTextNode.classList.add("hide");
+		helpTextNode.classList.remove("show");
+	}
+}
 
 // Disable/enable button
 submitButton.disabled = false; // default behaviour
